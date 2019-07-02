0797a1c0eb3a4a77729fb35b4911be91f6623bfd
superbderrick <kang.derrick@gmail.com>
2019-07-02 21:37:20 +0900

    restart in my home
    restart in my home

--- Assets/Scripts/FlipIt.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class FlipIt : MonoBehaviour
-{
-	public void Flip()
-	{
-		transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-	}
-}
